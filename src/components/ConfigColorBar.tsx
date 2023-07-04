9e519f09dea0a8e309b7c1a8cf34fccd60de7329
Toxicom <tomas.98.leo@gmail.com>
2023-07-04 16:08:38 +0300

    remove obsolete style object

--- src/components/ConfigColorBar.tsx
+++ src/components/ConfigColorBar.tsx
@@ -8,26 +8,6 @@ import { Color, ColorPicker, toColor, useColor } from 'react-color-palette';
 
 import "react-color-palette/lib/css/styles.css";
 
-const inputStyles = {
-  outline: 'none',
-  boxShadow: 'inset 0px 2px 0px rgba(225,228,232,0.2)',
-  boxSizing: 'border-box',
-  backgroundColor: '#ffffff',
-  border: '1px solid #CFD9E0',
-  borderRadius: '6px',
-  color: '#414D63',
-  fontFamily: '-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol',
-  fontSize: '0.875rem',
-  lineHeight: '1.25rem',
-  padding: '10px 0.75rem',
-  margin: 0,
-  cursor: 'auto',
-  width: '100%',
-  zIndex: 1,
-  height: '40px',
-  maxHeight: '40px',
-}
-
 export const ConfigColorBar = ({ id, label, hexColor, onChange }: any) => {
   const [color, setColor] = useColor('hex', hexColor);
   const [isOpen, setIsOpen] = useState(false);
